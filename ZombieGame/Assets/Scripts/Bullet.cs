12c246675452268f5687bae0c86b805bbbc2de77
lee.J.Y <168066172+Doeunho@users.noreply.github.com>
2024-05-06 02:02:49 +0900

    총알 ui

--- ZombieGame/Assets/Scripts/Bullet.cs
+++ ZombieGame/Assets/Scripts/Bullet.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] TextMeshProUGUI bullettext;
+    [SerializeField] int currentbullet;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
